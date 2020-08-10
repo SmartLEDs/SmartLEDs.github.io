68ab7d805e784cf2d3342d7f9ae3b6f0808ee099
SmartLEDs <69111269+SmartLEDs@users.noreply.github.com>
2020-08-10 23:11:29 +0300

    nineth commit

--- js/scriptCal.js
+++ js/scriptCal.js
@@ -7,9 +7,9 @@ let div3 = document.querySelector('.out3');
 
 button.onclick = function () {
 	let b = +inputIn.value;
-	div.innerHTML = "Количество метров: " + Math.round(b*0.017*100)/100 + " метра(ов)";
-	div1.innerHTML = "Подсветка + гирлянда: " + Math.ceil(b * 3.33 + 1491);
-	div2.innerHTML = "Цветомузыка + гирлянда: " + Math.ceil(b * 3.33 + 1491 * 1.6);
-	div3.innerHTML = "Гирлянда на елку: " + Math.ceil(b * 3.33 + 1285);
+	div.innerHTML = "Количество метров: " + Math.round(b*(5/300)*100)/100 + " метра(ов)";
+	div1.innerHTML = "Подсветка + гирлянда: " + Math.ceil(b * (1000/300) + 1491);
+	div2.innerHTML = "Цветомузыка + гирлянда: " + Math.ceil(b * (1000/300) + 1491 * 1.53);
+	div3.innerHTML = "Гирлянда на елку: " + Math.ceil(b * (1000/300) + 1285);
 	inputIn.value = '';
 }
