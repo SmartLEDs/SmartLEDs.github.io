5dc0b83d5b29b8529fefe8d7b0bfa1aaeaa75e58
SmartLEDs <69111269+SmartLEDs@users.noreply.github.com>
2020-08-14 22:10:32 +0300

    10 commit

--- js/scriptCal.js
+++ js/scriptCal.js
@@ -5,11 +5,15 @@ let div1 = document.querySelector('.out1');
 let div2 = document.querySelector('.out2');
 let div3 = document.querySelector('.out3');
 
+function roundToMultiple(num, multiple) {
+	return Math.round(num/multiple) * multiple;
+}
+
 button.onclick = function () {
 	let b = +inputIn.value;
 	div.innerHTML = "Количество метров: " + Math.round(b*(5/300)*100)/100 + " метра(ов)";
-	div1.innerHTML = "Подсветка + гирлянда: " + Math.ceil(b * (1000/300) + 1491);
-	div2.innerHTML = "Цветомузыка + гирлянда: " + Math.ceil(b * (1000/300) + 1491 * 1.53);
-	div3.innerHTML = "Гирлянда на елку: " + Math.ceil(b * (1000/300) + 1285);
+	div1.innerHTML = "Подсветка + гирлянда: " + roundToMultiple((b * (1000/300) + 1250),10); //Math.ceil()
+	div2.innerHTML = "Цветомузыка + гирлянда: " + roundToMultiple((b * (1000/300) + 1250 * 1.53),10);
+	div3.innerHTML = "Гирлянда на елку: " + roundToMultiple((b * (1000/300) + 999),10);
 	inputIn.value = '';
 }
